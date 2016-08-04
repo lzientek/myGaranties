27d391aa9e01b638032a38087810080dd9f755b0
lzientek <lucas@zientek.fr>
2016-08-04 21:55:37 +0200

    changement vers du postgresql ajout de devise pour le login utilisateurs je suis amoureux

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160702104947) do
+ActiveRecord::Schema.define(version: 20160804184554) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "garanties", force: :cascade do |t|
     t.string   "product_name"
@@ -23,15 +26,24 @@ ActiveRecord::Schema.define(version: 20160702104947) do
     t.integer  "user_id"
   end
 
-  add_index "garanties", ["user_id"], name: "index_garanties_on_user_id"
+  add_index "garanties", ["user_id"], name: "index_garanties_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "lastname"
     t.string   "firstname"
-    t.string   "email"
-    t.string   "password"
-    t.datetime "creation_date"
-    t.datetime "last_connection_date"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
